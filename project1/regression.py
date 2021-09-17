0e9bd32c62f1f10bcd2a40bdb311be33fac599ca
haakooto <hakonolav.torvik@gmail.com>
2021-09-17 16:42:02 +0200

    implement regressions methods

--- project1/regression.py
+++ project1/regression.py
@@ -1,4 +1,17 @@
 import numpy as np
+from sklearn import linear_model
 
-def OLS():
-    pass
+
+def OLS(X, z, lmd=0):
+    return np.linalg.pinv(X.T @ X) @ X.T @ z
+
+
+def Ridge(X, z, lmd):
+    return np.linalg.pinv(X.T @ X - lmd * np.eye(X.shape[1])) @ X.T @ z
+
+
+def Lasso(X, z, lmd):
+    model = linear_model.Lasso(lmd)
+    model.fit(X, z)
+    return model.coef_
+
