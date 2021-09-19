6434b1ce45059e4d63af6f9760ddb896e546f7e9
haakooto <hakonolav.torvik@gmail.com>
2021-09-19 21:15:50 +0200

    write short func for plotting MSE as func of max plygrad

--- project1/code/analysis.py
+++ project1/code/analysis.py
@@ -1,13 +1,29 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import sys
-import os
-import time
+import utils
+import resampling
+import regression
 
 
-def main():
-    pass
+def tmp_func_name(args):
+    N = args.num_points
+    P = args.polynomial  # polynomial degrees
 
+    x = np.sort(np.random.uniform(size=N))
+    y = np.sort(np.random.uniform(size=N))
+    x, y = np.meshgrid(x, y)
+    z = utils.FrankeFunction(x, y, eps0=args.epsilon).flatten()
+    MSEs = np.zeros(len(P))
 
-if __name__ == "__main__":
-    main()
+    for i, p in enumerate(P):
+        print("p =", p)
+        X = utils.create_X(x, y, p)
+
+        reg_meth = eval(f"regression.{args.method}")
+        if args.resampling != "None":
+            print("No resampling methods have been implemented")
+
+        MSEs[i] = resampling.NoResampling(X, z, args.tts, args.resampling_iter, args.lmb, reg_meth)
+
+    plt.plot(P, MSEs)
+    plt.show()
