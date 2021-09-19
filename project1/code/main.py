9f3a6308f7b8279af19ef72fafc134d204e542ce
haakooto <hakonolav.torvik@gmail.com>
2021-09-19 21:16:15 +0200

    remove unused code

--- project1/code/main.py
+++ project1/code/main.py
@@ -1,6 +1,3 @@
-# import argparse
-# import ord_lstsq, plot
-import numpy as np
 import utils
 from analysis import tmp_func_name
 
@@ -9,10 +6,6 @@ def main():
     args = utils.parse_args()
 
     tmp_func_name(args)
-    # x = np.sort(np.random.uniform(size=n))
-    # y = np.sort(np.random.uniform(size=n))
-    # Model = ord_lstsq.Regression((x, y), ord_lstsq.FrankeFunction, P=p, eps0=0, scaling="S")
-    # Model.fit()
 
 if __name__ == "__main__":
     main()
