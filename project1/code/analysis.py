311b4c2381585546e99fb22380abfbe390c07252
haakooto <hakonolav.torvik@gmail.com>
2021-09-21 11:19:36 +0200

    stop using evals

--- project1/code/analysis.py
+++ project1/code/analysis.py
@@ -1,29 +1,44 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler, MinMaxScaler, Normalizer
 import utils
-import resampling
 import regression
+import resampling
 
+
+class NoneScaler(StandardScaler):
+    """ To have the option of no scaling of the data """
+    def transform(self, X):
+        return X
+
+reg_conv = {"OLS": regression.Ordinary_least_squares, "Ridge": regression.Ridge, "Lasso":regression.Lasso}
+resampling_conv = {"None": resampling.NoResampling, "Boot": resampling.Bootstrap, "CV": resampling.cross_validation}
+scale_conv = {"None": NoneScaler(), "S": StandardScaler(), "N": Normalizer(), "M": MinMaxScaler()}
 
 def tmp_func_name(args):
     N = args.num_points
     P = args.polynomial  # polynomial degrees
+    scaler = scale_conv[args.scaling]
 
     x = np.sort(np.random.uniform(size=N))
     y = np.sort(np.random.uniform(size=N))
     x, y = np.meshgrid(x, y)
     z = utils.FrankeFunction(x, y, eps0=args.epsilon).flatten()
     MSEs = np.zeros(len(P))
+    MSE_train = np.zeros(len(P))
 
     for i, p in enumerate(P):
         print("p =", p)
         X = utils.create_X(x, y, p)
-
-        reg_meth = eval(f"regression.{args.method}")
+        
         if args.resampling != "None":
             print("No resampling methods have been implemented")
+        else:
+            resampl = resampling_conv[args.resampling]
 
-        MSEs[i] = resampling.NoResampling(X, z, args.tts, args.resampling_iter, args.lmb, reg_meth)
-
-    plt.plot(P, MSEs)
+        data = resampl(X, z, args.tts, args.resampling_iter, args.lmb, reg_conv[args.method], scaler)
+        MSEs[i] = data["test_MSE"] 
+        MSE_train[i] = data["train_MSE"] 
+    plt.plot(P, MSEs, "bo--")
+    plt.plot(P, MSE_train, "ro--")
     plt.show()
