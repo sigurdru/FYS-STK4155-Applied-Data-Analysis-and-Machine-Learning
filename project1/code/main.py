f13030cb107643d48d39fb758b8b47c431a435dd
haakooto <hakonolav.torvik@gmail.com>
2021-09-29 19:31:49 +0200

    update main

--- project1/code/main.py
+++ project1/code/main.py
@@ -1,15 +1,16 @@
 import utils
-from analysis import bias_var_tradeoff, no_resamp
+from analysis import bias_var_tradeoff, lambda_BVT, simple_regression
 
 utils.np.random.seed(136)
 
 
 def main():
     args = utils.parse_args()
-    if args.resampling == "None":
-        no_resamp(args)
-    else:
-        bias_var_tradeoff(args)
+    # if args.resampling == "None":
+    #     simple_regression(args)
+    # else:
+    #     bias_var_tradeoff(args)
+    lambda_BVT(args)
 
 if __name__ == "__main__":
     main()
