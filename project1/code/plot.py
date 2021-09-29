ae5523a9fde69c869f0b6c46329fc1032c492230
haakooto <hakonolav.torvik@gmail.com>
2021-09-29 09:26:45 +0200

    make Plot_BVT take in 1 dict with datas, insteadof 6 arrays, add func for showing directly, many small changes

--- project1/code/plot.py
+++ project1/code/plot.py
@@ -27,10 +27,10 @@ def set_ax_info(ax, xlabel, ylabel, title=None, zlabel=None):
         title (str): the desired title on the axis
         xlabel (str): the desired lab on the x-axis
         ylabel (str): the desired lab on the y-axis
-        zlabel (str): the desired lab on the z-axis 
+        zlabel (str): the desired lab on the z-axis
 
     """
-    if zlabel == None:
+    if zlabel is None:
         ax.set_xlabel(xlabel, fontsize=20)
         ax.set_ylabel(ylabel, fontsize=20)
         ax.set_title(title, fontsize=20)
@@ -43,20 +43,32 @@ def set_ax_info(ax, xlabel, ylabel, title=None, zlabel=None):
         ax.set_zlabel(zlabel, fontsize=18)
         ax.tick_params(axis='both', which='major', labelsize=12)
 
-def Plot_FrankeFunction(fname):
+
+def show(fig, fname, args):
+    fig.savefig(os.path.join(path_plots, fname + '.pdf'))
+    if args.show:
+        plt.show()
+    else:
+        plt.clf()
+
+
+def Plot_FrankeFunction(x, y, z, args):
     """Plot the Franke function and saves the plot in the output
     folder
 
     Args:
-        fname (str): name of the output file
+        x (array):  x-data
+        y (array):  y-data
+        z (array):  z-values
+        args (argparse): argparse containing info about run
     """
+
+    if 1 in z.shape:
+        n = int(np.sqrt(z.shape[0]))
+        z = z.reshape((n, n))
+
     fig = plt.figure()
     ax = fig.gca(projection="3d")
-    # Make data.
-    x = np.arange(0, 1, 0.05)
-    y = np.arange(0, 1, 0.05)
-    x, y = np.meshgrid(x, y)
-    z = utils.FrankeFunction(x, y)
     # Plot the surface.
     surf = ax.plot_surface(x, y, z, cmap=cm.coolwarm,
                        linewidth=0, antialiased=False)
@@ -67,16 +79,16 @@ def Plot_FrankeFunction(fname):
 
     # Add a color bar which maps values to colors.
     fig.colorbar(surf, shrink=0.5, aspect=5)
-
     #general formalities
+    fname = f"Frak_anal_eps_{args.epsilon}"
     title = 'Analytical plot of Franke\'s function'
     xlabel = '$x$'
     ylabel = '$y$'
     zlabel = '$f$'
     set_ax_info(ax, xlabel, ylabel, title, zlabel)
     fig.tight_layout()
-    fig.savefig(os.path.join(path_plots, fname + '.pdf'))
-    plt.close()
+    show(fig, fname, args)
+
 
 def Plot_error(MSE_test, MSE_train, args):
     """Plot mean square error as a function of polynomial degree
@@ -87,26 +99,27 @@ def Plot_error(MSE_test, MSE_train, args):
         MSE_train (array): array of train mean square error
         args (argparse): argparse containing information of method used
     """
-    #Plot the data
+    # Plot the data
     fig, ax = plt.subplots()
     ax.plot(args.polynomial, MSE_test, "bo--", label="test MSE")
     ax.plot(args.polynomial, MSE_train, "ro--", label="Train MSE")
-    #general formalities
+
+    # general formalities
     fname = 'MSE_' + args.method \
             + '_n' + str(args.num_points) \
-            + '_eps' +str(args.epsilon) \
-            + '_pol' +str(max(args.polynomial))
-    #So we dontget any dots in the fnames
-    fname = fname.replace('.','-')
+            + '_eps' + str(args.epsilon) \
+            + '_pol' + str(max(args.polynomial))
+    fname = fname.replace('.','-')  # remove dots from fname
     title = 'Mean square error for ' \
             + args.method
     xlabel = 'Polynomial degree'
     ylabel = 'MSE'
     set_ax_info(ax, xlabel, ylabel, title)
-    #save figure
-    fig.savefig(os.path.join(path_plots, fname + '.pdf'))
-    print('Plotting error: See '+ fname + '.pdf')
-    plt.close()
+
+    # save figure
+    print('Plotting error: See ' + fname + '.pdf')
+    show(fig, fname, args)
+
 
 def Plot_R2(R2_test, R2_train, args):
     """Plot R^2 score as a function of polynomial degree
@@ -117,65 +130,76 @@ def Plot_R2(R2_test, R2_train, args):
         R2_train (array): array of train R2 score
         args (argparse): argparse containing information of method used
     """
-    #Plot the data
+    # Plot the data
     fig, ax = plt.subplots()
     ax.plot(args.polynomial, R2_test, "bo--", label="Test R2")
     ax.plot(args.polynomial, R2_train, "ro--", label="Train R2")
-    #general formalities
+    # general formalities
     fname = 'R2_' + args.method \
             + '_n' + str(args.num_points) \
             + '_eps' + str(args.epsilon) \
             + '_pol' + str(max(args.polynomial))
-    #So we dontget any dots in the fnames
-    fname = fname.replace('.', '-')
+    fname = fname.replace('.', '-') # remove dots from fname
     title = 'R2 score for ' \
             + args.method
     xlabel = 'Polynomial degree'
     ylabel = 'R2'
     set_ax_info(ax, xlabel, ylabel, title)
-    #save figure
-    fig.savefig(os.path.join(path_plots, fname + '.pdf'))
-    print('Plotting R2: See ' + fname + '.pdf')
-    plt.close()
 
-def Plot_bias_var_tradeoff(test_errors, test_biases, test_vars, train_errors,
-                            train_biases, train_vars, args):
+    # save figure
+    print('Plotting R2: See ' + fname + '.pdf')
+    show(fig, fname, args)
+
+
+def Plot_bias_var_tradeoff(datas, args):
     """Plot mean square error, variance and error as a function of polynomial degree
 
     Args:
-        test_errors  (array) array containing test error
-        test_biases  (array) array containing test bias
-        test_vars    (array) array containing test variance
-        train_errors (array) array containing train error
-        train_biases (array) array containing train bias
-        train_vars   (array) array containing train variance
+        datas (defaultdict) contains following arrays
+                test_errors
+                test_biases
+                test_vars
+                train_errors
+                train_biases
+                train_vars
         args (argparse): argparse containing information of method used
     """
     print('Plotting Bias variance tradeoff: See output file.')
-    #make figure and plot data
+    # make figure and plot data
     fig, ax = plt.subplots()
-    
+
     P = args.polynomial
-    ax.plot(P, test_errors, "bo-", label="test Error")
-    ax.plot(P, test_biases, "ro-", label="test Bias")
-    ax.plot(P, test_vars, "go-", label="test Variance")
-    ax.plot(P, train_errors, "bo--", label="train Error")
-    ax.plot(P, train_biases, "ro--", label="train Bias")
-    ax.plot(P, train_vars, "go--", label="train Variance")
-    #General formalities
+    ax.plot(P, datas["test_errors"], "bo-", label="test Error")
+    ax.plot(P, datas["test_biases"], "ro-", label="test Bias")
+    ax.plot(P, datas["test_vars"], "go-", label="test Variance")
+    ax.plot(P, datas["train_errors"], "bo--", label="train Error")
+    ax.plot(P, datas["train_biases"], "ro--", label="train Bias")
+    ax.plot(P, datas["train_vars"], "go--", label="train Variance")
+    # General formalities
     xlabel = 'Polynomial degree'
     ylabel = 'Bias, variance and error'
     title = 'Bias Variance Tradeoff for ' + args.resampling
     set_ax_info(ax, xlabel, ylabel, title)
-    #Saving figure
-    fname = fname = 'BVT_' + args.method \
+    # Saving figure
+    fname = 'BVT_' + args.method \
             + '_n' + str(args.num_points) \
-            + '_eps' + str(args.epsilon) 
-    fig.savefig(os.path.join(path_plots, fname + '.pdf'))
-    plt.close()
+            + '_eps' + str(args.epsilon)
+    show(fig, fname, args)
+
 
 if __name__ == "__main__":
     """
     Testing
     """
-    Plot_FrankeFunction("Frank_anal")
+    class Argparse:
+        def __init__(self):
+            self.show = True
+            self.epsilon = 0
+
+    N = 30
+    x = np.sort(np.random.uniform(size=N))
+    y = np.sort(np.random.uniform(size=N))
+    x, y = np.meshgrid(x, y)
+    z = utils.FrankeFunction(x, y)
+    args = Argparse()
+    Plot_FrankeFunction(x, y, z, args)
