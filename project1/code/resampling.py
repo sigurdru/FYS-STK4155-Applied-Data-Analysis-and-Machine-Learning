Fixing 1D BV-tradeoff testing, corrected scaling

project1/code/resampling.py
@@ -16,10 +16,6 @@ def split_scale(X, z, ttsplit, scaler):
     Returns:
         X_train, X_test, z_train, z_test, 2darrays: Scaled train and test data
     """
-    scaler.fit(X)
-    X = scaler.transform(X)
-    scaler.fit(z)
-    z = scaler.transform(z)
 
     if ttsplit != 0:
         X_train, X_test, z_train, z_test = tts(X, z, test_size=ttsplit)
@@ -32,9 +28,10 @@ def split_scale(X, z, ttsplit, scaler):
     scaler.fit(X_train)
     X_train = scaler.transform(X_train)
     X_test = scaler.transform(X_test)
-    if scaler.__str__() == "StandardScaler()":
-        z_test = (z_test - np.mean(z_train)) / np.std(z_train)
-        z_train = (z_train - np.mean(z_train)) / np.std(z_train)
+
+    scaler.fit(z_train)
+    z_train = scaler.transform(z_train)
+    z_test = scaler.transform(z_test)
 
     return X_train, X_test, z_train, z_test
 

project1/code/test.py
@@ -11,7 +11,7 @@ import regression
 import plot
 import utils
 
-utils.np.random.seed(2018)
+utils.np.random.seed(7132)
 plt.style.use('seaborn')
 plt.rc('text', usetex=True)
 plt.rc('font', family='DejaVu Sans')
@@ -223,11 +223,11 @@ def test_BV():
     Test bias variance tradeoff using bootstrap
     """
     #Variables we are going to use
-    N = 200
-    maxdegree = 30
-    P = np.arange(1,maxdegree+1,1)
+    N = 170
+    maxdegree = 16
+    P = np.arange(0,maxdegree,1)
     eps = 0.1
-    resampling = "Boot"
+    resampling_m = "Boot"
     ttsplit = 0.2
     resampling_iter = 100
     lmb = [0.1]
@@ -235,23 +235,27 @@ def test_BV():
     scaling_conv = "S"
     #Using our method
     args = params(N, P, eps,
-                resampling, ttsplit, resampling_iter,
+                resampling_m, ttsplit, resampling_iter,
                 lmb, method, scaling_conv)
     our_results = analysis.bias_var_tradeoff(args, testing=True)
 
 
     #Using sklearn
-    utils.np.random.seed(2018)
+    utils.np.random.seed(7132)
     from sklearn.pipeline import make_pipeline
-    from sklearn.preprocessing import PolynomialFeatures
+    from sklearn.preprocessing import PolynomialFeatures, StandardScaler
     from sklearn.utils import resample
+    # import resampling
     # x = np.linspace(-3,3,N).reshape(-1,1)
     x = np.sort(np.random.uniform(-3, 3, size=N)).reshape(-1,1)
     y = f(x, eps)
     error = np.zeros(maxdegree)
     bias = np.zeros(maxdegree)
     variance = np.zeros(maxdegree)
-    x_train, x_test, y_train, y_test = tts(x, y, test_size=0.2)
+    scaler = StandardScaler()
+    x_train, x_test, y_train, y_test = resampling.split_scale(x, y, ttsplit=0.2, scaler=StandardScaler())
+    # x_train, x_test, y_train, y_test = tts(x, y, test_size=0.2)
+
 
     for degree in range(maxdegree):
         model = make_pipeline(PolynomialFeatures(degree=degree), LinearRegression(fit_intercept=False))
@@ -274,7 +278,6 @@ def test_BV():
     plt.plot(P, our_results["test_vars"], '--', label='Our Variance')
     plt.plot(P, our_results["test_biases"], '--', label='Our bias')
 
-    plt.ylim(-0.05, 5)
     plt.legend()
     plt.show()
 

project1/code/utils.py
@@ -261,8 +261,8 @@ def create_X(x, y, n):
             Includes intercept.
     """
     if type(y) == int:
-        X = np.zeros((len(x), n))
-        for i in range(n):
+        X = np.zeros((len(x), n+1))
+        for i in range(n+1):
             X[:, i] = x**i
         return X
     if not 1 in x.shape: