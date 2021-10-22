1e123f67bedb1f21c13c94ddd3be19223063403a
Sigurd Rustad <sigurdsr@gmail.com>
2021-10-22 19:23:51 +0200

    progress

--- project2/code/test.py
+++ project2/code/test.py
@@ -1,11 +1,17 @@
 import numpy as np
-eta = "np.linspace(0,1,10)"
-command = "eta_val = " + eta
-print(command)
-exec(command)
-print(eta_val)
-# eta = "2"
-# eta_val = exec(eta)
+# eta = "np.linspace(0,1,10)"
+# command = "eta_val = " + eta
+# print(command)
+# exec(command)
 # print(eta_val)
-a = 1
-print(np.shape(a) == ())
+# # eta = "2"
+# # eta_val = exec(eta)
+# # print(eta_val)
+# a = 1
+# print(np.shape(a) == ())
+from sklearn.model_selection import train_test_split as tts
+
+a = np.arange(0,10)
+a_test, a_train = tts(a)
+print(a_test)
+print(a_train)
