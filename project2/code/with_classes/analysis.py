c8445f0b5296980397b607175e4e1e065e6a4706
Vetle Vikenes <vetle.vkns@gmail.com>
2021-10-28 15:15:56 +0200

    Temporary. plots 3D of NN prediction

--- project2/code/with_classes/analysis.py
+++ project2/code/with_classes/analysis.py
@@ -13,6 +13,7 @@ from collections import defaultdict
 from sklearn.metrics import accuracy_score
 import seaborn as sns
 import matplotlib.pyplot as plt
+from matplotlib import cm
 
 class NoneScaler(StandardScaler):
     """ 
@@ -68,7 +69,6 @@ def analyse(args):
     x, y, z = utils.load_data(args)
     X = utils.create_X(x, y, p)
     X_train, X_test, z_train, z_test = split_scale(X, z, args.tts, scaler)
-
     data = defaultdict(lambda: np.zeros((len(etas), len(lmbs)), dtype=float))
     for i, eta in enumerate(etas):
         for j, lmb in enumerate(lmbs):
@@ -85,17 +85,49 @@ def analyse(args):
             train_pred = NN.predict(X_train)
             test_pred = NN.predict(X_test)
 
+            fig = plt.figure()
+            ax = fig.gca(projection="3d")
+            # Plot the surface.
+            # print(np.shape(z))
+            # print(np.shape(train_pred))
+            # exit()
+            z = NN.predict(X).reshape((x.shape[0], x.shape[1]))
+            surf = ax.plot_surface(x, y, z, cmap=cm.coolwarm,
+                                linewidth=0, antialiased=False)
+            # Customize the z axis.
+            # ax.zaxis.set_major_locator(LinearLocator(10))
+            # ax.zaxis.set_major_formatter(FormatStrFormatter("%.02f"))
+
+            # Add a color bar which maps values to colors.
+            fig.colorbar(surf, shrink=0.5, aspect=5)
+            plt.show()
+            exit()
+
+            OLS_beta = np.linalg.pinv(X_train.T @ X_train) @ X_train.T @ z_train
+            OLS_train_pred = X_train @ OLS_beta 
+            OLS_test_pred = X_test @ OLS_beta
+
             print('eta={:.2f}: MSE_test={:.3f}'.format(eta, MSE(z_test,test_pred)[0]))
             data["train_accuracy"][i][j] = MSE(z_train, train_pred)
             data["test_accuracy"][i][j] = MSE(z_test, test_pred)
+            data["OLS_train"][i][j] = MSE(z_train, OLS_train_pred)
+            data["OLS_test"][i][j] = MSE(z_test, OLS_test_pred)
             # exit()
+
+            # plt.plot()
+    # print(data["train_accuracy"]);exit()
+
     for name, accuracy in data.items():
-        fig, ax = plt.subplots()
-        sns.heatmap(accuracy, ax=ax, annot=True)
-        ax.set_title(name)
-        ax.set_xlabel("$\eta$")
+        # exit()
+        plt.plot(etas, data[name], 'o-', label=name)
+        # fig, ax = plt.subplots()
+        # sns.heatmap(accuracy, ax=ax, annot=True)
+        # ax.set_title(name)
+        # ax.set_xlabel("$\eta$")
         # ax.setylabel("$\lambda$")
-        plt.show()
+        # plt.show()
+    plt.legend()
+    plt.show()
 
 def MSE(z, ztilde):
     return sum((z - ztilde)**2) / len(z)
