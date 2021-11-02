3d1929ce75e241f80ff3f5d05e4873f2159b6f83
haakooto <hakonolav.torvik@gmail.com>
2021-11-02 17:22:16 +0100

    make classification work. Still shit

--- project2/code/analysis.py
+++ project2/code/analysis.py
@@ -77,7 +77,7 @@ def NN_regression(args):
 def linear_regression(args):
     p = args.polynomial
     etas = args.eta
-    lmbs = args.lmb 
+    lmbs = args.lmb
 
     if args.batch_size == 0:
         batch_sizes = np.array([720, 360, 240, 144, 72, 48, 30, 24])
@@ -119,31 +119,31 @@ def linear_regression(args):
     if not args.gamma:
         """
         Plot MSE heatmaps for SGD without momentum
-        Different parameters are plotted based on inputs 
+        Different parameters are plotted based on inputs
 
         Number of epochs vs eta values:
          - lmb: default (float)
-         - eta: array-like 
+         - eta: array-like
          - bs : default (int)
 
         Number of epochs vs number of minibatches:
          - lmb: default (float)
          - eta: 0.3 (float)
-         - bs : 0 
+         - bs : 0
 
         Lambda values vs eta values:
-         - lmb: Array-like 
-         - eta: Array-like  
+         - lmb: Array-like
+         - eta: Array-like
          - bs : default (int)
         """
         for name, accuracy in data.items():
             fig, ax = plt.subplots()
             if len(lmbs) == 1 and len(batch_sizes) == 1:
                 """
-                (x,y): (epochs, eta values) 
+                (x,y): (epochs, eta values)
 
                 Good simulation:
-                - Ne  : 150 
+                - Ne  : 150
                 - eta : np.linspace(0.01, 0.05, 31)
                 """
                 cols = np.arange(args.num_epochs)
@@ -160,43 +160,43 @@ def linear_regression(args):
                 (x,y): (epochs, number of minibatches)
 
                 Good simulation:
-                - Ne : 150 
-                - eta: 0.3 
-                - bs : 0 
+                - Ne : 150
+                - eta: 0.3
+                - bs : 0
                 """
                 cols = np.arange(args.num_epochs)
-                idx = n_minibatches 
+                idx = n_minibatches
                 data = pd.DataFrame(accuracy[0,0,:,:], index=idx, columns=cols[:])
                 ylabel = "Number of minibatches"
                 xlabel = "Number of epochs"
                 title = name + " for Franke function, using $\eta=0.3$"
-                ytick = idx 
-                vmax = 0.07 
+                ytick = idx
+                vmax = 0.07
 
             if len(lmbs) != 1 and len(n_minibatches) == 1:
                 """
                 (x,y): (log10[lambda], eta values)
 
                 Good simulation:
-                - Ne  : 200 
+                - Ne  : 200
                 - lmb : np.logspace(-5, 0, 11)
                 - eta : np.linspace(0.1, 0.7, 11)
                 """
-                cols = np.log10(lmbs) 
+                cols = np.log10(lmbs)
                 idx = np.round(etas, 3)
                 data = pd.DataFrame(accuracy[:,:,0,-1], index=idx, columns=cols[:])
                 ylabel = "Learning rate $\eta$"
                 xlabel = "$\log_{10}(\lambda)$"
                 title = name + " for Franke function after 200 epochs, using 20 minibatches"
-                ytick = idx 
-                vmax = None 
+                ytick = idx
+                vmax = None
 
 
-            ax = sns.heatmap(data, 
-                            ax=ax, 
-                            annot=False, 
-                            cmap=cm.coolwarm, 
-                            vmax=vmax, 
+            ax = sns.heatmap(data,
+                            ax=ax,
+                            annot=False,
+                            cmap=cm.coolwarm,
+                            vmax=vmax,
                             linewidths=0,
                             xticklabels=len(cols)//10,
                             yticklabels=ytick)
@@ -213,10 +213,10 @@ def linear_regression(args):
             plt.plot(data["Train MSE"].flatten(), label='Train MSE. $\gamma=0$')
             plt.plot(data["Test MSE"].flatten() , label='Test MSE. $\gamma=0$')
 
-            mom_MSE_train, mom_MSE_test = SGD.SGD((X_train, X_test), 
-                                                (z_train, z_test), 
-                                                args, 
-                                                beta0, 
+            mom_MSE_train, mom_MSE_test = SGD.SGD((X_train, X_test),
+                                                (z_train, z_test),
+                                                args,
+                                                beta0,
                                                 eta,
                                                 batch_size,
                                                 lmb,
@@ -233,27 +233,22 @@ def logistic_regression(args):
 
 
 def NN_classification(args):
-    if args.dataset == "Cancer":
-        cancer = utils.load_data(args)
-        X, z = cancer.data, cancer.target.reshape(-1, 1)
-        # z = z.reshape(-1, 1)
-        print(X.shape)
-        print(z.shape)
-        for i, eta in enumerate(args.eta):
-            for j, lmb in enumerate(args.lmb):
-                NN = FFNN(X,
-                          z,
-                          hidden_nodes=args.hidden_nodes,
-                          batch_size=args.batch_size,
-                          learning_rate=eta,
-                          lmb=lmb,
-                          clas=True,
-                          activation="sigmoid",
-                          cost="accuracy",
-                          )
-                NN.train(args.num_epochs)
-                print(NN.weights)
+    dataset = utils.load_data(args)
+    X, z_ = dataset.data, dataset.target.reshape(-1, 1)
+    z = utils.categorical(z_)
+    NN = FFNN(X,
+              z,
+              hidden_nodes=args.hidden_nodes,
+              batch_size=args.batch_size,
+              learning_rate=args.eta[0],
+              lmb=args.lmb[0],
+              clas=True,
+              activation="sigmoid",
+              cost="accuracy",
+              )
+    NN.train(args.num_epochs)
+    probs = NN.predict(X)
+    pred = np.argmax(probs, axis=1).reshape(-1, 1)
+    print(sum(z_))
+    print(np.sum(pred == z_) / len(z_))
 
-    else:
-        train, test = utils.load_data(args)
-        # X_train, z_train = train.
