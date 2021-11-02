801494f06c37563cf6a4fa0ae9b38f3b7ed17150
Vetle Vikenes <vetle.vkns@gmail.com>
2021-11-02 08:12:26 +0100

    Removed old code

--- project2/code/with_classes/SGD.py
+++ project2/code/with_classes/SGD.py
@@ -1,10 +1,6 @@
 import numpy as np
 from numpy.lib import utils
 
-# class Regression:
-#     def __init__(self, args):
-#         beta = np.random.randn(utils.get_features(p))
-
 class Regression:
     def __init__(self):
         return None 
@@ -25,11 +21,14 @@ def SGD(X, z, args, beta, eta, gamma=0, lmb=0):
     Returns:
         total_gradient 1darray: total gradient
     """
-
-    M = args.minibatch  # size of minibatch
     n = int((args.num_points**2)*(1-args.tts))  # number of datapoints for testing
-    v = 0 
+    if args.batch_size == None:
+        M = n 
+    else:
+        M = args.batch_size  # size of minibatch
 
+    m = n // M
+    v = 0 
     X_train, X_test = X 
     z_train, z_test = z 
 
@@ -37,6 +36,7 @@ def SGD(X, z, args, beta, eta, gamma=0, lmb=0):
 
     MSE_train = np.zeros(args.num_epochs)
     MSE_test  = np.zeros(args.num_epochs)
+    eta_0 = eta 
 
     for epoch_i in range(args.num_epochs):
         # Initialize randomized training data for epoch 
@@ -47,16 +47,10 @@ def SGD(X, z, args, beta, eta, gamma=0, lmb=0):
         for i in range(0, n, M):
             # Loop over mini batches 
 
-            # random_index = np.random.randint(n-M)
-            # xi = X_train[random_index:random_index + M]
-            # zi = z_train[random_index:random_index + M]
-
             xi = X_train_shuffle[i:i+M]
             zi = z_train_shuffle[i:i+M]
 
-            # Dividing by M to get correct gradient 
-            # Using zi.T[0] instead of zi, such that gradients have the same shape as beta 
-            gradient = 2.0 * xi.T @ ((xi @ beta)-zi.T[0]) / M \
+            gradient = 2 * xi.T @ ((xi @ beta)-zi.T[0]) / M \
                         + 2 * lmb * beta 
 
             v = v * gamma + eta * gradient
@@ -68,7 +62,7 @@ def SGD(X, z, args, beta, eta, gamma=0, lmb=0):
         MSE_train[epoch_i] = MSE(z_train.T[0], train_pred)
         MSE_test[epoch_i] = MSE(z_test.T[0], test_pred)
 
-    # eta = learning_schedule(epoch_i*m + i,args)
+        # eta = learning_schedule(epoch_i*m + i,args)
 
     return MSE_train, MSE_test 
 
@@ -77,14 +71,17 @@ def RidgeSG():
     return None 
 
 
-# def learning_schedule(t, args):
-#     if args.learning_schedule == True:
-#         t0 = 1
-#         t1 = 10
-#         # return args.t0/(t+args.t1)
-#         return t0/(t+t1)
-#     else:
-#         return args.eta
+def learning_schedule(t, args):
+    # if args.learning_schedule == True:
+    #     t0 = 1
+    #     t1 = 10
+    #     # return args.t0/(t+args.t1)
+    #     return t0/(t+t1)
+    # else:
+    #     return args.eta
+    t0 = 10
+    t1 = 1
+    return t0/(t+t1)
 
 
 def MSE(y, y_pred):
