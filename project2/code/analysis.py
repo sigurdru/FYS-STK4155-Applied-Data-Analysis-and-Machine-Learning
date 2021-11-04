Fixed scaling of Franke data. Added option for plotting 3D fit

project2/code/analysis.py
@@ -29,8 +29,7 @@ class NoneScaler(StandardScaler):
 scale_conv = {"None": NoneScaler(),
                 "S": StandardScaler(),
                 "N": Normalizer(), 
-                "M": MinMaxScaler(),
-                "S_Franke": StandardScaler(with_std=False)}
+                "M": MinMaxScaler()}
 
 
 def NN_regression(args):
@@ -42,7 +41,6 @@ def NN_regression(args):
     X = utils.create_X(x, y, p, intercept=False if p == 1 else True)
     X_train, X_test, z_train, z_test = utils.split_scale(X, z, args.tts, scaler)
 
-
     data = defaultdict(lambda: np.zeros((len(etas), len(lmbs)), dtype=float))
     for i, eta in enumerate(etas):
         for j, lmb in enumerate(lmbs):
@@ -53,20 +51,51 @@ def NN_regression(args):
                       learning_rate=eta,
                       lmb=lmb,
                       )
-            NN.train(args.num_epochs)
+            hist, err = NN.train(args.num_epochs, train_history=False)
 
-            train_pred = NN.predict(X_train)
-            test_pred = NN.predict(X_test)
+            # Rescale data to obtain correct values    
+            train_pred = utils.rescale_data(NN.predict(X_train), z)
+            test_pred = utils.rescale_data(NN.predict(X_test), z)
 
-            tr_mse = utils.MSE(z_train, train_pred)
-            te_mse = utils.MSE(z_test, test_pred)
+            z_train_ = utils.rescale_data(z_train, z)
+            z_test_ = utils.rescale_data(z_test, z)
+
+            tr_mse = utils.MSE(z_train_, train_pred)
+            te_mse = utils.MSE(z_test_, test_pred)
+            
+            print('mse train  : ', tr_mse)
+            print('mse test   : ', te_mse)
+
+            # plt.plot(range(args.num_epochs), hist)
+            # plt.show()
 
             data["train_MSE"][i][j] = tr_mse if tr_mse < 1 else np.nan
             data["test_MSE"][i][j] = te_mse if te_mse < 1 else np.nan
 
-    print("\n"*3)
-    print(f"Best NN train prediction: {(train:=data['train_MSE'])[(mn:=np.unravel_index(np.nanargmin(train), train.shape))]} for eta = {np.log10(etas[mn[0]])}, lambda = {lmbs[mn[1]]}")
-    print(f"Best NN test prediction: {(test:=data['test_MSE'])[(mn:=np.unravel_index(np.nanargmin(test), test.shape))]} for eta = {np.log10(etas[mn[0]])}, lambda = {lmbs[mn[1]]}")
+            if args.pred:
+                """
+                Should be inputs to a separate plot script in the future
+                """
+                X_ = utils.split_scale(X, z, 0, scaler)[0] # Scale design matrix 
+                z_ = utils.split_scale(X, z, 0, scaler)[-1]# Scale targets 
+
+                # Calculate output. Rescale values to the original 
+                z_pred = utils.rescale_data(NN.predict(X_), z)
+                z_pred_ = z_pred.reshape((x.shape[0], x.shape[1]))
+
+                z_target = z.reshape((x.shape[0], x.shape[1]))
+
+                fig = plt.figure()
+                ax = fig.gca(projection="3d")
+                surf = ax.plot_surface(x,y,z_target, alpha=0.3, cmap=cm.coolwarm, linewidth=0, antialiased=False)
+                surf = ax.plot_surface(x,y,z_pred_, cmap=cm.jet, linewidth=0, antialiased=False)
+
+                fig.colorbar(surf, shrink=0.5, aspect=5)
+                plt.show()
+
+    # print("\n"*3)
+    # print(f"Best NN train prediction: {(train:=data['train_MSE'])[(mn:=np.unravel_index(np.nanargmin(train), train.shape))]} for eta = {np.log10(etas[mn[0]])}, lambda = {lmbs[mn[1]]}")
+    # print(f"Best NN test prediction: {(test:=data['test_MSE'])[(mn:=np.unravel_index(np.nanargmin(test), test.shape))]} for eta = {np.log10(etas[mn[0]])}, lambda = {lmbs[mn[1]]}")
 
     for name, accuracy in data.items():
         fig, ax = plt.subplots()
@@ -87,38 +116,59 @@ def linear_regression(args):
         batch_sizes = np.array([720, 360, 240, 144, 72, 48, 30, 24])
     else:
         batch_sizes = np.array([args.batch_size])
-
+    # scaler = StandardScaler(with_std=False)
     scaler = scale_conv[args.scaling]
     x, y, z = utils.load_data(args)
-    X = utils.create_X(x, y, p)
+    X = utils.create_X(x, y, p, intercept=False if p == 1 else True)
 
     X_train, X_test, z_train, z_test = utils.split_scale(X, z, args.tts, scaler)
 
     n_minibatches = X_train.shape[0] // batch_sizes
     ols_beta = np.linalg.pinv(X_train.T @ X_train) @ X_train.T @ z_train
-    ols_pred = X_test @ ols_beta
-
 
     data = defaultdict(lambda: np.zeros((len(etas),
                                         len(lmbs),
                                         len(batch_sizes),
                                         args.num_epochs), dtype=float))
 
-    beta0 = np.random.randn(utils.get_features(p))
+    beta0 = np.random.randn(utils.get_features(p)) 
 
     for i, eta in enumerate(etas):
         for j, lmb in enumerate(lmbs):
             for k, batch_size in enumerate(batch_sizes):
-                MSE_train, MSE_test = SGD.SGD((X_train, X_test),
+                MSE_train, MSE_test, beta = SGD.SGD((X_train, X_test),
                                                 (z_train, z_test),
                                                 args,
                                                 beta0,
                                                 eta,
                                                 batch_size,
-                                                lmb)
+                                                lmb,
+                                                args.gamma)
 
                 data["Train MSE"][i][j][k] = MSE_train
                 data["Test MSE"][i][j][k] = MSE_test
+
+                if args.pred:
+                    """
+                    Should be inputs to a separate plot script in the future
+                    """
+                    X_ = utils.split_scale(X, z, 0, scaler)[0] # Scale design matrix 
+
+
+                    # Calculate output. Rescale values to the original 
+                    z_pred = utils.rescale_data(X_ @ beta, z)
+                    z_pred_ = z_pred.reshape((x.shape[0], x.shape[1]))
+
+                    z_target = z.reshape((x.shape[0], x.shape[1]))
+
+                    fig = plt.figure()
+                    ax = fig.gca(projection="3d")
+                    surf = ax.plot_surface(x,y,z_target, alpha=0.3, cmap=cm.coolwarm, linewidth=0, antialiased=False)
+                    surf = ax.plot_surface(x,y,z_pred_, cmap=cm.jet, linewidth=0, antialiased=False)
+
+                    fig.colorbar(surf, shrink=0.5, aspect=5)
+                    plt.show()
+                exit()
 
     if not args.gamma:
         """
@@ -217,7 +267,7 @@ def linear_regression(args):
             plt.plot(data["Train MSE"].flatten(), label='Train MSE. $\gamma=0$')
             plt.plot(data["Test MSE"].flatten() , label='Test MSE. $\gamma=0$')
 
-            mom_MSE_train, mom_MSE_test = SGD.SGD((X_train, X_test),
+            mom_MSE_train, mom_MSE_test, beta = SGD.SGD((X_train, X_test),
                                                 (z_train, z_test),
                                                 args,
                                                 beta0,
@@ -257,7 +307,8 @@ def NN_classification(args):
               cost="cross_entropy",
               output_activation="softmax"
               )
-
+    # NN.train(args.num_epochs)
+    # exit()
     prob_hist, prob_err = NN.train(args.num_epochs, train_history=True)
     train_output = NN.predict(X_train)
     train_pred = np.argmax(train_output, axis=1)
@@ -271,7 +322,7 @@ def NN_classification(args):
     print("\n"*2)
 
     # Plot of accuracy as a function of epochs 
-    plt.plot(np.arange(args.num_epochs), prob_hist)
+    plt.plot(np.arange(args.num_epochs), prob_hist, 'o-')
     plt.show()
 
     # Plot of error in output as a function of epochs