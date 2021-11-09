389369f9c83a14936e3da595c75511105cdf67e0
haakooto <hakonolav.torvik@gmail.com>
2021-11-09 17:39:58 +0100

    fix tex in titles

--- project2/code/plot.py
+++ project2/code/plot.py
@@ -26,7 +26,7 @@ def show_push_save(fig, func, args):
     """
     This function handles wether you want to show,
     save and/or push the file.
-    
+
     Args:
         fig (matplotlib.figure): Figure you want to handle
         func (string):  function sending fig, for easier fname creation
@@ -60,7 +60,7 @@ def set_fname(func, args):
     fname += "_" + func["z"]  # varying parameter
     fname += "__" + str(int(np.random.uniform() * 1e6))  # random number to identify plot
     fname += ".pdf"
-    
+
     # save configuration to file
     if args.save:
         with open(archive, "a") as file:
@@ -77,9 +77,9 @@ def set_fname(func, args):
 def surface_fit(data_pred, data_target, x, y, args):
     """
     Plot Franke function
-    Compares fit result with data using epsilon=0.05 
+    Compares fit result with data using epsilon=0.05
     """
-    
+
     z_pred_ = data_pred.reshape(x.shape)
     z_target = data_target.reshape(y.shape)
 
@@ -89,9 +89,9 @@ def surface_fit(data_pred, data_target, x, y, args):
     surf = ax.plot_surface(x,y,z_pred_, cmap=cm.jet, linewidth=0, antialiased=False)
 
     if args.method == "NN":
-        title = "Franke function prediction with Neural Network and noise $\epsilon=N(0,\,0.05)$"
+        title = r"Franke function prediction with Neural Network and noise $\epsilon=N(0,\,0.05)$"
     else:
-        title = "Franke function prediction with SGD and noise $\epsilon=N(0,\,0.05)$"
+        title = r"Franke function prediction with SGD and noise $\epsilon=N(0,\,0.05)$"
 
     ax.set_xlabel('x')
     ax.set_ylabel('y')
@@ -136,12 +136,12 @@ def parameter_based(data, args):
             cols = np.arange(args.num_epochs)
             idx = np.round(args.eta, 3)
             data = pd.DataFrame(accuracy[:,0,0,:], index=idx, columns=cols[:])
-            ylabel = "Learning rate $\eta$"
+            ylabel = r"Learning rate $\eta$"
             xlabel = "Number of epochs"
             title = name + " for Franke function, using 20 minibatches"
             ytick = 3
             vmax = 0.07
-            
+
             func["x"] = "epochs"
             func["y"] = "eta"
             func["z"] = "MSE"
@@ -160,7 +160,7 @@ def parameter_based(data, args):
             data = pd.DataFrame(accuracy[0,0,:,:], index=idx, columns=cols[:])
             ylabel = "Number of minibatches"
             xlabel = "Number of epochs"
-            title = name + " for Franke function, using $\eta=0.3$"
+            title = name + r" for Franke function, using $\eta=0.3$"
             ytick = idx
             vmax = 0.07
 
@@ -180,8 +180,8 @@ def parameter_based(data, args):
             cols = np.log10(args.lmb)
             idx = np.round(args.eta, 3)
             data = pd.DataFrame(accuracy[:,:,0,-1], index=idx, columns=cols[:])
-            ylabel = "Learning rate $\eta$"
-            xlabel = "$\log_{10}(\lambda)$"
+            ylabel = r"Learning rate $\eta$"
+            xlabel = r"$\log_{10}(\lambda)$"
             title = name + " for Franke function after 200 epochs, using 20 minibatches"
             ytick = idx
             vmax = None
@@ -211,9 +211,9 @@ def momentum(data, args):
         func = defaultdict(lambda: None)
         func["train"] = name.split()[0]
         fig, ax = plt.subplots()
-        
-        for i, mse in enumerate(accuracy):    
-            ax.plot(mse, label=f'{name}. $\gamma={args.gamma[i]:.2f}$')
+
+        for i, mse in enumerate(accuracy):
+            ax.plot(mse, label=rf'{name}. $\gamma={args.gamma[i]:.2f}$')
         ax.set_title(name + "during training for different momentums")
         ax.set_xlabel("Number of epochs")
         ax.set_ylabel("MSE")
@@ -241,8 +241,8 @@ def eta_lambda(data, args, NN=False):
             ax.set_title(name + f" gridsearch, using {args.act_func.replace('_', ' ')} activation function on {args.dataset}-data")
         else:
             ax.set_title(name + f"as function of learning rate and lambda for {args.dataset}-data")
-        ax.set_ylabel("$\eta$")
-        ax.set_xlabel("$\lambda$")
+        ax.set_ylabel(r"$\eta$")
+        ax.set_xlabel(r"$\lambda$")
         show_push_save(fig, func, args)
 
 def train_history(NN, args):
