Fixed scaling for Franke SGD. Changed gamma array for momentum analysis

project2/code/analysis.py
@@ -102,11 +102,17 @@ def linear_regression(args):
     p = args.polynomial
     etas = args.eta
     lmbs = args.lmb
-    scaler = scale_conv[args.scaling]
 
     if args.pred:
         # Reduce noise for surface plot comparison
         args.epsilon = 0.05
+
+    if args.scaling == "S":
+        # Don't divide data by std for Franke 
+        # Since std<1, causing too much increase of data values  
+        scaler = StandardScaler(with_std=False)
+    else:
+        scaler = scale_conv[args.scaling]
 
     x, y, z = utils.load_data(args)
     X = utils.create_X(x, y, p, intercept=False if p == 1 else True)
@@ -115,7 +121,7 @@ def linear_regression(args):
     beta0 = np.random.randn(utils.get_features(p))  # use same beta0 in all runs for comparisonability
     if args.gamma >= 0:
         if args.batch_size == 0:
-            batch_sizes = np.array([720, 360, 240, 144, 72, 48, 30, 24])
+            batch_sizes = np.array([720, 360, 240, 144, 72, 48, 36, 30, 24])
         else:
             batch_sizes = np.array([args.batch_size])
         args.batch_size = batch_sizes
@@ -149,18 +155,18 @@ def linear_regression(args):
                         """
                         Plot fitted prediction vs data set with epsilon=0.05
                         """
+                        # Scale the full design matrix 
                         X_ = utils.split_scale(X, z, 0, scaler)[0] # Scale design matrix
 
-
-                        # Calculate output. Rescale values to the original
-                        z_pred = utils.rescale_data(X_ @ beta, z)
+                        # Calculate prediction. Rescale values 
+                        z_pred = X_ @ beta + np.mean(z)
 
                         plot.surface_fit(z_pred, z, x, y, args)
 
         plot.parameter_based(data, args)
 
     else:
-        gammas = np.array([0, 0.25, 0.5, 0.9])
+        gammas = np.linspace(0, 0.95, 20)
         args.gamma = gammas
         data = defaultdict(lambda: np.zeros((len(gammas), args.num_epochs)))
 