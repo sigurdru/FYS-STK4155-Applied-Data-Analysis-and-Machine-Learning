964d0c94f49655be21f692c7cd55e2a09a134087
haakooto <hakonolav.torvik@gmail.com>
2021-11-17 12:22:07 +0100

    add description

--- project2/code/utils.py
+++ project2/code/utils.py
@@ -1,3 +1,6 @@
+"""
+Many useful support functions, mainly to treat data
+"""
 import numpy as np
 from sklearn.model_selection import train_test_split as tts
 from sklearn.datasets import load_breast_cancer, load_digits
