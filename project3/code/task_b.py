f6407d652d3f7efbf616995ec54574de1ef8670a
Vetle Nevland <vetlene@student.matnat.uio.no>
2021-11-23 11:19:52 +0100

    Add space step test

--- project3/code/task_b.py
+++ project3/code/task_b.py
@@ -4,19 +4,24 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--dx', type=float, default=0.2)
-parser.add_argument('--dt', type=float, default=0.5)
+parser.add_argument('--dt', type=float, default=0.01)
 parser.add_argument('--T', type=float, default=1.0)
+parser.add_argument('--exact', type=bool, default=True)
+parser.add_argument('--nr_plots', type=int, default=5)
 a = parser.parse_args()
+
 dx = a.dx
 dt = a.dt
 L = 1
 T = a.T
+exact = a.exact
+nr_plots = a.nr_plots
 
 def IC(x):
     return np.sin(np.pi*x)
 
 u_exact = lambda x, t: np.exp(-np.pi**2*t)*np.sin(np.pi*x)
-u_store = [] # To store solution for each time step
+u_store = {} # To store solution for each time step
 
 def forward_euler(L, T, IC, BC_l, BC_r, dx, dt, user_action=None):
     #dt = 0.4*dx**2 # Ensure stability
@@ -51,33 +56,28 @@ def forward_euler(L, T, IC, BC_l, BC_r, dx, dt, user_action=None):
     return u_m, x, t
 
 
-def visualize(L, T, dx, dt, IC, BC_l, BC_r, nr_plots=10, exact=False):
-    """User action function"""
-    def plot_sol(u, x, t, t_idx):
 
-        if t_idx % int((T/dt)/nr_plots) == 0: # Only plot nr_plots curves
-            plt.plot(x, u, label=f't={t_idx}')
-            if exact:
-                plt.plot(x, u_exact(x, t[t_idx]), '--', label=f't_e={t_idx}') 
+def plot_sols(u, x, t, t_idx):
+    global nr_plots, exact
+    plot_idx = np.linspace(0, int(round(t[-1]/(t[1]-t[0]))), nr_plots)
 
-        if t_idx == len(t)-1:
-            plt.legend()
-            plt.show()
+    if t_idx in plot_idx: # Only plot nr_plots curves
+        plt.plot(x, u, label=f't={t_idx}')
+        if exact:
+            plt.plot(x, u_exact(x, t[t_idx]), '--', label=f't_e={t_idx}') 
 
-    u, x, t = forward_euler(L, T, IC, BC_l, BC_r, dx, dt, user_action=plot_sol)
-    return u
+    if t_idx == len(t)-1:
+        plt.legend()
+        plt.show()
 
 def print_solution(u, x, t, t_idx):
-    """Use action function"""
     print(f'Solution at time step {t_idx}:', u)
 
 def store_solution(u, x, t, t_idx):
-    """User action function"""
     global u_store
-    u_store.append(u)
+    u_store[t_idx] = u
 
 def assert_local_error(u, x, t, t_idx):
-    """User action function"""
     u_e = u_exact(x, t[t_idx])
     error = np.abs(u - u_e).max()
     tol = 1e-8
@@ -93,18 +93,28 @@ def total_integrated_error(u, x, t):
         E += e2
     
     E *= dt 
-    return np.sqrt(E)
+    return np.round(np.sqrt(E), 3)
+
+def test_space_steps():
+    """Tests numerical scheme for time step dx=1/10
+    and dx=1/100 and compares with analytical solution."""
+    L = 1
+    T = 0.5
+    for dx in 0.1, 0.01:
+        dt = 0.5*dx**2 # Satisfies stability criteria
+        #u, x, t = forward_euler(L, T, IC, 0, 0, dx, dt, user_action=plot_sols)
+        u, x, t = forward_euler(L, T, IC, 0, 0, dx, dt, user_action=store_solution)
+
+        E = total_integrated_error(u_store, x, t)
+        print(f'Total integrated error for dx={dx}:', E)
+
 
 if __name__ == '__main__':
-    L = 1
-    T = 1
     BC_l = 0
     BC_r = 0
 
-    u_sol = visualize(L, T, dx, dt, IC, BC_l, BC_r, nr_plots=5, exact=True)
-    #u, x, t = forward_euler(L, T, IC, BC_l, BC_r, dx, dt, user_action=print_solution)
-    u, x, t = forward_euler(L, T, IC, BC_l, BC_r, dx, dt, user_action=store_solution)
+    u, x, t = forward_euler(L, T, IC, BC_l, BC_r, dx, dt, user_action=plot_sols)
+    #u, x, t = forward_euler(L, T, IC, BC_l, BC_r, dx, dt, user_action=store_solution)
+    test_space_steps()
 
-    E = total_integrated_error(u_store, x, t)
-    print('Total integrated error', E)
 
